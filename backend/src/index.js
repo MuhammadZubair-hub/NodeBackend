da8e3ab9f761cc8d6444d35c3b207a79ec25172a
zubair <mianmuhammadzubair94@gmial.com>
2025-11-24 17:13:40 +0500

    changed

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,28 +4,47 @@ import jokesRouter from './routes/jokes.js';
 import productRouter from './routes/product.js';
 import mongoose from 'mongoose';
 import orderRouter from './routes/order.js';
-import { ConnectDB } from './db/index.js';
+// import { ConnectDB } from './db/index.js';
 
 dotenv.config({
-  path :'./.env'
+  path: './.env'
 })
 
 
 const app = express();
-const port = 5000
+
 app.use(express.json());
 
-app.use('/api',jokesRouter);
-app.use('/api/p',productRouter);
-app.use('/api/o',orderRouter);
+app.use('/api', jokesRouter);
+app.use('/api/p', productRouter);
+app.use('/api/o', orderRouter);
 
 
 
-ConnectDB().then(()=>{
-  app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`)
-});
-}).catch((e)=>{console.log(e)})
+// ConnectDB().then(() => {
+//   app.listen(process.env.PORT, () => {
+//     console.log(`Example app listening on port ${process.env.PORT}`)
+//   });
+// }).catch((e) => { console.log(e) });
+
+
+
+const ConnectDB = async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGO_URI}/${process.env.DB_Name}`);
+    console.log('Monog DB conncects yar  || ', process.env.DB_Name);
+    app.listen(process.env.PORT, () => {
+      console.log(`Example app listening on port ${process.env.PORT}`)
+    })
+
+  } catch (error) {
+    console.log('Mongo DB conncects nai ho yar  || ');
+
+  }
+};
+
+
+ConnectDB();
 
 
 
